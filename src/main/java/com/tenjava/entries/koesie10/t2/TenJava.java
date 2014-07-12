277d80c8e14d0ef5224395654e58869164a27604
Koen Vlaswinkel <koen@vlaswinkel.info>
2014-07-12 11:23:48 +0200

    Add PlayerListener with test PlayerJoinEvent

--- src/main/java/com/tenjava/entries/koesie10/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/koesie10/t2/TenJava.java
@@ -1,5 +1,13 @@
 package com.tenjava.entries.koesie10.t2;
 
+import com.tenjava.entries.koesie10.t2.listeners.PlayerListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(this), this);
+    }
+}
---
+++ src/main/java/com/tenjava/entries/koesie10/t2/listeners/PlayerListener.java
@@ -0,0 +1,25 @@
+package com.tenjava.entries.koesie10.t2.listeners;
+
+import com.tenjava.entries.koesie10.t2.TenJava;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerListener implements Listener {
+    private TenJava plugin;
+
+    public PlayerListener(TenJava plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(final PlayerJoinEvent e) {
+        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+            @Override
+            public void run() {
+                e.getPlayer().sendMessage("Welcome to this minigame server!");
+            }
+        }, 10);
+    }
+}
